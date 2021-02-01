69a1e40de43a6bea31709d73552fb94f94ed1258
Imdad <imdadadelabou0@gmail.com>
2021-02-01 23:21:16 +0100

    push master

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const path = require('path')
 const app = express()
+var requiredMessage = "Le champ ne doit pas etre vide"
 
 app.set('views', path.join(__dirname))
 app.set("view engine", "ejs")
@@ -23,7 +24,16 @@ app.get('/inscription', (req, res) => {
 })
 
 app.post('/inscription', [
-  check('UserEmail', 'Email invalid').isEmail(),
+  check('UserNom').notEmpty().withMessage(requiredMessage),
+  check('UserEmail').notEmpty().withMessage(requiredMessage)
+  .isEmail().withMessage("Email invalid"),
+  check('UserPrenom').notEmpty().withMessage(requiredMessage),
+  check('UserAge').notEmpty().withMessage(requiredMessage)
+  .matches(/^[0-9]/).withMessage("Age invalid"),
+  check('UserPassword').notEmpty().withMessage(requiredMessage)
+  .isLength({min: 8}).withMessage("Le mot de passe doit contenir au moins 8 caractères, un 1 nombre et une lettre")
+  .matches(/\S*(\S*([a-zA-Z]\S*[0-9])|([0-9]\S*[a-zA-Z]))\S*/)
+  .withMessage("Le mot de passe doit contenir au moins 1 nombre et un caractère special"),
 ], (req, res) => {
   const errors = validationResult(req);
 
